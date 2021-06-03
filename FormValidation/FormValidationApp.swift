23b4d02e8abe78c85b514ef498824ad42d339317
Phanit Pollavith <ppollavith@gmail.com>
2021-06-03 15:01:45 -0700

    Initial Commit

---
+++ FormValidation/FormValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormValidationApp.swift
+//  FormValidation
+//
+//  Created by Phanit Pollavith on 6/3/21.
+//
+
+import SwiftUI
+
+@main
+struct FormValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
